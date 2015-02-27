5d14df495b0c90c803ca2302d268eaafc77b3efa
huyang <daoxila@young4ever.local>
2015-02-27 09:40:52 +0800

    Initial Commit

---
+++ BlockTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlockTest
+//
+//  Created by huyang on 15/2/27.
+//  Copyright (c) 2015年 huyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlockTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlockTest
+//
+//  Created by huyang on 15/2/27.
+//  Copyright (c) 2015年 huyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
